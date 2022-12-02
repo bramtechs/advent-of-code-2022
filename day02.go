a47e64016673d2e5cd5c04ffa7688bf68541b747
brambasiel <bramtech@telenet.be>
2022-12-02 10:08:57 +0100

    day 2 complete

--- day02.go
+++ day02.go
@@ -27,22 +27,72 @@ func getRoundScore(you rune, opp rune) int {
 		score = 2
 	case 'Z':
 		score = 3
+	default:
+		panic("Unexpected char")
 	}
 	score += getBonusScore(you, opp)
 	// fmt.Println(string(you), string(opp), score)
 	return score
 }
 
+func simulateLosing(opp rune) rune {
+	switch opp {
+	case 'A':
+		return 'Z'
+	case 'B':
+		return 'X'
+	case 'C':
+		return 'Y'
+	default:
+		panic("Unexpected char")
+	}
+}
+
+func simulateWinning(opp rune) rune {
+	switch opp {
+	case 'A':
+		return 'Y'
+	case 'B':
+		return 'Z'
+	case 'C':
+		return 'X'
+	default:
+		panic("Unexpected char")
+	}
+}
+
+func simulateDraw(opp rune) rune {
+	return opp + 23 // see ascii table
+}
+
+func getSimulatedScore(opp rune, outcome rune) int {
+	you := '?'
+	switch outcome {
+	case 'X':
+		you = simulateLosing(opp)
+	case 'Y':
+		you = simulateDraw(opp)
+	case 'Z':
+		you = simulateWinning(opp)
+	default:
+		panic("Unexpected char")
+	}
+	return getRoundScore(you, opp)
+}
+
 func main() {
 	content := readFile("input02")
 	lines := strings.Split(content, "\n")
 	score := 0
+	score2 := 0
 	for _, line := range lines {
 		if len(line) > 0 {
 			you := rune(line[2])
 			opp := rune(line[0])
 			score += getRoundScore(you, opp)
+			score2 += getSimulatedScore(opp, you)
 		}
 	}
 	fmt.Println(score)
+	fmt.Println(score2)
 }
