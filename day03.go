8cfef2a2270704398a94e7954c63d8dfc7dabc57
brambasiel <bramtech@telenet.be>
2022-12-03 14:25:08 +0100

    day 3 finished

--- day03.go
+++ day03.go
@@ -27,6 +27,30 @@ func getDupes(first_str string, second_str string) []rune {
 	return dupes
 }
 
+func findCommon(first_str string, second_str string, third_str string) rune {
+	for _, c1 := range first_str {
+		for _, c2 := range second_str {
+			if c1 == c2 {
+				for _, c3 := range third_str {
+					if c1 == c3 {
+						return c1
+					}
+				}
+			}
+		}
+	}
+	panic("No common badge found")
+}
+
+func getPriority(char rune) int {
+	if char >= 'a' && char <= 'z' {
+		return int(char-'a') + 1
+	} else if char >= 'A' && char <= 'Z' {
+		return int(char - 'A' + 27)
+	}
+	panic(fmt.Sprint("Can't get priority of character ", char))
+}
+
 func main() {
 	content := readFile("input03")
 	sacks := strings.Split(content, "\n")
@@ -45,15 +69,18 @@ func main() {
 
 		// calculate priority
 		for _, dupe := range dupes {
-			// lowercase
-			if dupe >= 'a' && dupe <= 'z' {
-				priority += int(dupe-'a') + 1
-			} else if dupe >= 'A' && dupe <= 'Z' {
-				priority += int(dupe - 'A' + 27)
-			} else {
-				panic(fmt.Sprint("Unparsable character ", dupe))
-			}
+			priority += getPriority(dupe)
 		}
 	}
 	fmt.Println(priority)
+
+	priority2 := 0
+	for i := 0; i < len(sacks)-1; i += 3 {
+		first := sacks[i+0]
+		second := sacks[i+1]
+		third := sacks[i+2]
+		badge := findCommon(first, second, third)
+		priority2 += getPriority(badge)
+	}
+	fmt.Println(priority2)
 }
