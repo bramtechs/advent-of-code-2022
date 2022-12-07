9e3db299c09dc799902b8712806254a05a61a0e6
brambasiel <bramtech@telenet.be>
2022-12-07 11:21:00 +0100

    07 collect line members

--- day07.cpp
+++ day07.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+typedef std::pair<std::string, std::string> Members;
+
 struct File
 {
     string name;
@@ -33,6 +35,28 @@ struct File
     }
 };
 
+Members str_get_members(const std::string &line, bool isCmd)
+{
+    int i = isCmd ? 2 : 0;
+    std::string first, second;
+    char c = line.at(i);
+    while (c != ' ')
+    {
+        first += c;
+        i++;
+        if (i >= line.length())
+        {
+            return {first, ""}; // cmd with no argument
+        }
+        c = line.at(i);
+    }
+    for (int j = i; j < line.length(); j++)
+    {
+        second += line.at(j);
+    }
+    return {first, second}; // output with two arguments
+}
+
 inline bool str_is_command(const std::string cmd)
 {
     assert(cmd.length() > 0);
@@ -43,13 +67,20 @@ int parse_command(int i, std::vector<string> &lines)
 {
     auto &line = lines.at(i);
     assert(str_is_command(line));
-    cout << line << endl;
+    // cout << line << endl;
+
+    // run command
+    auto mem = str_get_members(line, true);
+    cout << mem.first << " : " << mem.second << endl;
+
     for (int j = i + 1; j < lines.size(); j++)
     {
         auto &out = lines.at(j);
         if (!str_is_command(out))
         {
-            cout << "|->" << out << endl;
+            // cout << "|->" << out << endl;
+            auto mem = str_get_members(out, false);
+            cout << "|-> " << mem.first << " : " << mem.second << endl;
         }
         else
         {
