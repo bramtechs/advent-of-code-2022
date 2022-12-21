dbddd331787285d018246621a847a3a1b2c6f701
brambasiel <bramtech@telenet.be>
2022-12-21 20:27:13 +0100

    finally parse the map

--- day03.go
+++ day03.go
@@ -1,86 +1,86 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "strings"
 )
 
 func runeSliceContains(slice []rune, match rune) bool {
-	for _, run := range slice {
-		if run == match {
-			return true
-		}
-	}
-	return false
+    for _, run := range slice {
+        if run == match {
+            return true
+        }
+    }
+    return false
 }
 
 func getDupes(first_str string, second_str string) []rune {
-	var dupes []rune
-	for _, first := range first_str {
-		for _, second := range second_str {
-			if first == second && !runeSliceContains(dupes, first) {
-				// also check if already registered
-				dupes = append(dupes, first)
-			}
-		}
-	}
-	return dupes
+    var dupes []rune
+    for _, first := range first_str {
+        for _, second := range second_str {
+            if first == second && !runeSliceContains(dupes, first) {
+                // also check if already registered
+                dupes = append(dupes, first)
+            }
+        }
+    }
+    return dupes
 }
 
 func findCommon(first_str string, second_str string, third_str string) rune {
-	for _, c1 := range first_str {
-		for _, c2 := range second_str {
-			if c1 == c2 {
-				for _, c3 := range third_str {
-					if c1 == c3 {
-						return c1
-					}
-				}
-			}
-		}
-	}
-	panic("No common badge found")
+    for _, c1 := range first_str {
+        for _, c2 := range second_str {
+            if c1 == c2 {
+                for _, c3 := range third_str {
+                    if c1 == c3 {
+                        return c1
+                    }
+                }
+            }
+        }
+    }
+    panic("No common badge found")
 }
 
 func getPriority(char rune) int {
-	if char >= 'a' && char <= 'z' {
-		return int(char-'a') + 1
-	} else if char >= 'A' && char <= 'Z' {
-		return int(char - 'A' + 27)
-	}
-	panic(fmt.Sprint("Can't get priority of character ", char))
+    if char >= 'a' && char <= 'z' {
+        return int(char-'a') + 1
+    } else if char >= 'A' && char <= 'Z' {
+        return int(char - 'A' + 27)
+    }
+    panic(fmt.Sprint("Can't get priority of character ", char))
 }
 
 func day03() {
-	content := readFile("input03")
-	sacks := strings.Split(content, "\n")
-	priority := 0
-	for _, sack := range sacks {
-		half := len(sack) / 2
-		first_half := sack[:half]
-		second_half := sack[half:]
+    content := readFile("input03")
+    sacks := strings.Split(content, "\n")
+    priority := 0
+    for _, sack := range sacks {
+        half := len(sack) / 2
+        first_half := sack[:half]
+        second_half := sack[half:]
 
-		// remove windows awfulness
-		second_half = strings.ReplaceAll(second_half, "\r", "")
+        // remove windows awfulness
+        second_half = strings.ReplaceAll(second_half, "\r", "")
 
-		// find duplicates
-		dupes := getDupes(first_half, second_half)
-		//fmt.Println(string(dupes))
+        // find duplicates
+        dupes := getDupes(first_half, second_half)
+        //fmt.Println(string(dupes))
 
-		// calculate priority
-		for _, dupe := range dupes {
-			priority += getPriority(dupe)
-		}
-	}
-	fmt.Println(priority)
+        // calculate priority
+        for _, dupe := range dupes {
+            priority += getPriority(dupe)
+        }
+    }
+    fmt.Println(priority)
 
-	priority2 := 0
-	for i := 0; i < len(sacks)-1; i += 3 {
-		first := sacks[i+0]
-		second := sacks[i+1]
-		third := sacks[i+2]
-		badge := findCommon(first, second, third)
-		priority2 += getPriority(badge)
-	}
-	fmt.Println(priority2)
+    priority2 := 0
+    for i := 0; i < len(sacks)-1; i += 3 {
+        first := sacks[i+0]
+        second := sacks[i+1]
+        third := sacks[i+2]
+        badge := findCommon(first, second, third)
+        priority2 += getPriority(badge)
+    }
+    fmt.Println(priority2)
 }
--- day14.d
+++ day14.d
@@ -62,9 +62,9 @@ Rectangle getMapBounds(Map map)
 
 void printMap(Map map, Rectangle bounds)
 {
-    for (int y = bounds.y1; y < bounds.y2; y++)
+    for (int y = bounds.y1; y <= bounds.y2; y++)
     {
-        for (int x = bounds.x1; x < bounds.x2; x++)
+        for (int x = bounds.x1; x <= bounds.x2; x++)
         {
             char letter = '.';
             Point coord = {x, y};
@@ -72,14 +72,14 @@ void printMap(Map map, Rectangle bounds)
             {
                 switch (*type)
                 {
-                case Tile.ROCK:
-                    letter = '#';
-                    break;
-                case Tile.SOURCE:
-                    letter = '+';
-                    break;
-                default:
-                    assert(0);
+                    case Tile.ROCK:
+                        letter = '#';
+                        break;
+                    case Tile.SOURCE:
+                        letter = '+';
+                        break;
+                    default:
+                        assert(0);
                 }
             }
             write(letter);
@@ -99,16 +99,55 @@ void main()
     Point source = {500, 0};
     map[source] = Tile.SOURCE;
 
-    // parse the map
+    // get rock lines map
     foreach (line; splitLines(content))
     {
+        Point[] segments;
         foreach (coord; split(line, " -> "))
         {
             string[] coords = split(coord, ',');
             assert(coords.length == 2);
-            Point p = {to!int(coords[0]),
-            to!int(coords[1])};
-            map[p] = Tile.ROCK;
+            Point p = {
+                to!int(coords[0]),
+                to!int(coords[1])
+            };
+
+            segments ~= p;
+        }
+
+        for(int i = 1; i < segments.length; i++){
+            Point p1 = segments[i-1];
+            Point p2 = segments[i];
+    
+
+            if (p1.x == p2.x){ // vertical
+                int x = p1.x;
+                if (p2.y < p1.y){
+                    int c = p1.y;
+                    p1.y = p2.y;
+                    p2.y = c;
+                }
+                assert(p1.y < p2.y);
+                for (int y = p1.y; y <= p2.y; ++y){
+                    Point p = { x, y };
+                    map[p] = Tile.ROCK;
+                }
+            }
+            else if (p1.y == p2.y){ // horizontal
+                int y = p1.y;
+                if (p2.x < p1.x){
+                    int c = p1.x;
+                    p1.x = p2.x;
+                    p2.x = c;
+                }
+                assert(p1.x < p2.x);
+                for (int x = p1.x; x <= p2.x; ++x){
+                    Point p = { x, y };
+                    map[p] = Tile.ROCK;
+                }
+            }else{
+                assert(false);
+            }
         }
     }
 
